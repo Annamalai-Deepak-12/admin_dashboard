b71c05dbcad2d521c61438dd5afa6a8699d4c817
Annamalai-Deepak-12 <annamalaideepakr@gmail.com>
2023-05-01 23:39:38 +0200

    context

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./sidebar";
 import "./css/sb-admin-2.css";
-import "./fontawesome-free/css/all.min.css"
+import "./fontawesome-free/css/all.min.css";
 import Topbar from "./topbar";
 import Dashboard from "./dashboard";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -11,25 +11,24 @@ import User from "./user";
 import Usercreate from "./usercreate";
 import ViewUser from "./view";
 import Edit from "./edit";
+import { useContext } from "react";
+import { UserContext } from "./usercontext";
+import Login from "./login";
+import PortalLayout from "./PortalLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <div id="wrapper">
-        <Sidebar></Sidebar>
-        <div id="content-wrapper" class="d-flex flex-column">
-          <div id="content">
-            <Topbar></Topbar>
-            <Routes>
-              <Route path="/dashboard" element={<Dashboard />}></Route>
-              <Route path="/user" element={<User />}></Route>
-              <Route path="/user_create" element={<Usercreate />}></Route>
-              <Route path="/user/:id" element={<ViewUser />}></Route>
-              <Route path="/edit/:id" element={<Edit />}></Route>
-            </Routes>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/portal" element={<PortalLayout />}>
+          <Route path="dashboard" element={<Dashboard />}></Route>
+          <Route path="user" element={<User />}></Route>
+          <Route path="user_create" element={<Usercreate />}></Route>
+          <Route path="user/:id" element={<ViewUser />}></Route>
+          <Route path="edit/:id" element={<Edit />}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/usercreate.js
+++ src/usercreate.js
@@ -14,12 +14,43 @@ function Usercreate() {
       dob: "",
       gender: "",
     },
-    onSubmit: async(values) => {
+    validate: (values) => {
+      let error = {};
+      if (!values.name) {
+        error.name = "Please enter a value";
+      }
+      if (values.name && (values.name.length <= 2 || values.name.length > 15)) {
+        error.name = "Username must be between 3 to 15 characters";
+      }
+      if (!values.email) {
+        error.email = "Please enter an email";
+      }
+
+      if (
+        values.email &&
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        error.email = "Please enter a valid email";
+      }
+      if(values.phone.toString().length !==10){
+        error.phone="Please enter a valid phone number"
+      }
+
+      let age = new Date().getFullYear() - parseFloat(values.dob.split("-")[0])
+      if(age<18){
+        error.dob = "You must be above 18"
+      }
+      return error;
+    },
+    onSubmit: async (values) => {
       try {
-        const userdata = await axios.post("https://644efb15b61a9f0c4d1a7e1a.mockapi.io/users",values);
-        alert("Success")
+        const userdata = await axios.post(
+          "https://644efb15b61a9f0c4d1a7e1a.mockapi.io/users",
+          values
+        );
+        alert("Success");
       } catch (error) {
-        alert("Error")
+        alert("Error");
       }
     },
   });
@@ -30,14 +61,24 @@ function Usercreate() {
         <div className="row">
           <div className="col-lg-6">
             <div className="form-group">
-              <label>Username</label>
+              <label>Username *</label>
               <input
                 name="name"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.name}
                 type={"text"}
-                className="form-control"
+                className={`form-control ${
+                  formik.touched.name && formik.errors.name ? "error-box" : ""
+                } ${
+                  formik.touched.name && !formik.errors.name
+                    ? "success-box"
+                    : ""
+                }`}
               />
+              {formik.touched.name && formik.errors.name ? (
+                <span style={{ color: "red" }}>{formik.errors.name}</span>
+              ) : null}
             </div>
           </div>
           <div className="col-lg-6">
@@ -46,10 +87,20 @@ function Usercreate() {
               <input
                 name="email"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.email}
                 type={"text"}
-                className="form-control"
+                className={`form-control ${
+                  formik.touched.email && formik.errors.email ? "error-box" : ""
+                } ${
+                  formik.touched.email && !formik.errors.email
+                    ? "success-box"
+                    : ""
+                }`}
               />
+              {formik.touched.email && formik.errors.email ? (
+                <span style={{ color: "red" }}>{formik.errors.email}</span>
+              ) : null}
             </div>
           </div>
           <div className="col-lg-4">
@@ -94,10 +145,20 @@ function Usercreate() {
               <input
                 name="phone"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.phone}
-                type={"text"}
-                className="form-control"
+                type={"number"}
+                className={`form-control ${
+                  formik.errors.phone ? "error-box" : ""
+                } ${
+                  formik.touched.phone && !formik.errors.phone
+                    ? "success-box"
+                    : ""
+                }`}
               />
+              {formik.touched.phone && formik.errors.phone ? (
+                <span style={{ color: "red" }}>{formik.errors.phone}</span>
+              ) : null}
             </div>
           </div>
           <div className="col-lg-4">
@@ -106,10 +167,20 @@ function Usercreate() {
               <input
                 name="dob"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.dob}
                 type={"date"}
-                className="form-control"
+                className={`form-control ${
+                  formik.errors.dob ? "error-box" : ""
+                } ${
+                  formik.touched.dob && !formik.errors.dob
+                    ? "success-box"
+                    : ""
+                }`}
               />
+              {formik.errors.dob ? (
+                <span style={{ color: "red" }}>{formik.errors.dob}</span>
+              ) : null}
             </div>
           </div>
           <div className="col-lg-4">
@@ -121,9 +192,9 @@ function Usercreate() {
                 value={formik.values.gender}
                 className="form-control"
               >
-                <option></option>
-                <option>Male</option>
-                <option>Female</option>
+                <option>---Select One---</option>
+                <option value={"male"}>Male</option>
+                <option value={"female"}>Female</option>
               </select>
             </div>
           </div>

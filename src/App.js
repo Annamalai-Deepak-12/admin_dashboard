414fb8c0d83b6f0f3e398ca27d60f2f83ce74c55
Annamalai-Deepak-12 <annamalaideepakr@gmail.com>
2023-05-02 00:56:29 +0200

    12

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Sidebar from "./sidebar";
 import "./css/sb-admin-2.css";
 import "./fontawesome-free/css/all.min.css";
-import Topbar from "./topbar";
 import Dashboard from "./dashboard";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import User from "./user";
 import Usercreate from "./usercreate";
 import ViewUser from "./view";
 import Edit from "./edit";
-import { useContext } from "react";
-import { UserContext } from "./usercontext";
 import Login from "./login";
 import PortalLayout from "./PortalLayout";
 
@@ -20,7 +15,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<Login />}></Route>
         <Route path="/portal" element={<PortalLayout />}>
           <Route path="dashboard" element={<Dashboard />}></Route>
           <Route path="user" element={<User />}></Route>

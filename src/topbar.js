b34da6de2c0c0841db1b4f0c15893354f6df26cf
Annamalai-Deepak-12 <annamalaideepakr@gmail.com>
2023-05-02 00:52:23 +0200

    commit1

--- src/topbar.js
+++ src/topbar.js
@@ -42,7 +42,6 @@ function Topbar() {
         <li class="nav-item dropdown no-arrow d-sm-none">
           <a
             class="nav-link dropdown-toggle"
-            href="#"
             id="searchDropdown"
             role="button"
             data-toggle="dropdown"
@@ -84,7 +83,6 @@ function Topbar() {
         <li class="nav-item dropdown no-arrow">
           <a
             class="nav-link dropdown-toggle"
-            href="#"
             id="userDropdown"
             role="button"
             data-toggle="dropdown"
@@ -104,7 +102,7 @@ function Topbar() {
             class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="userDropdown"
           >
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" >
               <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
               Profile
             </a>
@@ -112,14 +110,13 @@ function Topbar() {
               <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
               Settings
             </a>
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" >
               <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
               Activity Log
             </a>
             <div class="dropdown-divider"></div>
             <a
               class="dropdown-item"
-              href="#"
               data-toggle="modal"
               data-target="#logoutModal"
             >
